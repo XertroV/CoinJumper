9c73eb4cd573348d371206ab93907386c193ec75
Max Kaye <m@xk.io>
2014-01-01 15:05:39 +1100

    more api work, still much untested

--- cj_api.py
+++ cj_api.py
@@ -30,10 +30,100 @@ def sha256Hash(plaintext):
 	h.update(plaintext)
 	return h.digest()
 	
-class Schema:
-	def __init__(self, name):
-		self.children = []
+def attribNameToIndex(attribName):
+	return 'index'+attribName[0].upper()+attribName[1:]
+	
+class DBNode:
+	def __init__(self, typeOfNode, name, valType=None, parent=None):
+		assert typeOfNode in ['static','variable','value'] # either it's a static name with subkeys, a variable name with subkeys, or holds some value (list/set/string/etc)
+		if parent == None: assert typeOfNode == 'static' # the only node without a parent is the first one and must be static\
+		assert valType in [None,'list','set','string']
+		assert ((typeOfNode == 'value') and (valType != None)) or (typeOfNode != 'value')
+		self.children = set()
+		self.value = ""
 		self.name = name
+		self.typeOfNode = typeOfNode
+		self.attribs = {}
+		self.parent = parent
+		self.valType = valType
+		self.aamc = self.addAndMakeChild
+		
+		if self.typeOfNode == 'variable':
+			self.parent.addAndMakeChild('value',attribNameToIndex(self.name),valType='list')
+		
+	def addChild(self, childNode):
+		if childNode not in self.children:
+			self.children.add(childNode)
+			self.attribs[childNode.name] = childNode
+			childNode.parent = self
+			
+	def addAndMakeChild(self, typeOfNode, name, valType=None):
+		self.addChild(DBNode(typeOfNode, name, valType=valType, parent=self))
+	
+	def getChildWithName(self, name):
+		for c in self.children:
+			if c.name == name:
+				return c
+		raise KeyError('No child named %s' % name)
+		
+	def printAll(self, subLevel=-1):
+		toPrint = ''
+		if subLevel == -1:
+			toPrint = self.name
+		else:
+			for sl in range(subLevel):
+				toPrint += '|'
+			toPrint += '-' 
+			template = '%s'
+			if self.typeOfNode == 'variable':
+				template = '<%s>'
+			toPrint += template % self.name
+		print toPrint
+		listOfChildren = self.attribs.keys()
+		listOfChildren.sort()
+		for cn in listOfChildren:
+			self.attribs[cn].printAll(subLevel=subLevel+1)
+	
+	def __getitem__(self, key):
+		return self.attribs[key]
+	
+	def __setitem__(self, name, node):
+		if not isinstance(node,DBNode):
+			raise ValueError('only DBNodes may be subchildren of DBNodes')
+		if name != node.name:
+			raise KeyError('name %s does not match node.name %s' % (name, node.name))
+		self.children.add(node)
+
+dbs = DBNode('static','cj')
+dbs.aamc('static','groups')
+dbs['groups'].aamc('variable','name')
+dbs['groups']['name'].aamc('value','class','string')
+dbs['groups']['name'].aamc('static','options')
+dbs['groups']['name']['options'].aamc('variable','optionName')
+dbs['groups']['name']['options']['optionName'].aamc('value','optionType','string')
+dbs['groups']['name']['options']['optionName'].aamc('value','listOfOptions','list')
+dbs['groups']['name']['options']['optionName'].aamc('value','default','string')
+dbs.aamc('static','items')
+dbs['items'].aamc('variable','class')
+dbs['items']['class'].aamc('variable','name')
+dbs['items']['class']['name'].aamc('value','description','string')
+dbs['items']['class']['name'].aamc('value','group','string')
+dbs['items']['class']['name'].aamc('value','price','string')
+dbs.aamc('static','invoices')
+dbs['invoices'].aamc('variable','id')
+dbs['invoices']['id'].aamc('value','network','string')
+dbs['invoices']['id'].aamc('value','totalAmount','string')
+dbs['invoices']['id'].aamc('value','confirmed','string')
+dbs['invoices']['id'].aamc('value','orderDetails','string')
+dbs.aamc('static','meta')
+dbs['meta'].aamc('value','version','string')
+dbs.aamc('static','config')
+dbs['config'].aamc('variable','network')
+dbs['config']['network'].aamc('value','hostport','string')
+dbs['config']['network'].aamc('value','username','string')
+dbs['config']['network'].aamc('value','password','string')
+dbs['config']['network'].aamc('value','xpub','string')
+#dbs.printAll()
 
 # database
 class Database:
@@ -54,14 +144,14 @@ class Database:
 		return self.r.rpush('%s:%s' % (self.dbPre,toPush), value)
 	# Complex Operations
 	def dump(self, pathToDump): # THIS IS UNTESTED - TEST!!!!
-		ans = []
+		ans = {}
 		attribPath = '%s:attribs' % pathToDump
 		if self.exists(attribPath):
 			listOfAttribs = self.get(attribPath)
 			for attrib in listOfAttribs:
 				if attrib[0] == '<' and attrib[-1] == '>':
 					attribName = attrib[1:-1]
-					iName = 'index'+attribName[0].upper()+attribName[1:]
+					iName = attribNameToIndex(attribName)
 					index = self.get('%s:%s' % (pathToDump, iName))
 					for n in index:
 						ans[n] = self.dump('%s:%s' % (pathToDump,n))
@@ -75,19 +165,13 @@ class Database:
 		menu['items'] = self.dump('items')
 		return menu
 
-'''# routes
+
+# routes
 @app.route("/api/get/menu")
 def get_menu():
 	menu = db.getMenu()
 	return json.dumps(menu)
 
-@app.route("/",methods=["GET","POST"])
-def main():
-	if request.method == "POST":
-		url = request.form['urlin']
-		link = db.addSite('http://' + url)
-		return render_template('result.html',url=url,link=link)
-	return render_template('index.html')'''
 
 if __name__ == "__main__":
 	db = Database()
